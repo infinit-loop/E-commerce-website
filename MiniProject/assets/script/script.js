5a9333751d34ea1c1488a468d5deafe63e492253
infinit-loop <39274593+infinit-loop@users.noreply.github.com>
2021-03-21 22:41:47 +0530

    Add files via upload

---
+++ MiniProject/assets/script/script.js
@@ -0,0 +1,31 @@
+const menu = document.querySelector(".menu");
+const navOpen = document.querySelector(".hamburger");
+const navClose = document.querySelector(".close");
+
+const navLeft = menu.getBoundingClientRect().left;
+navOpen.addEventListener("click", () => {
+  if (navLeft < 0) {
+    menu.classList.add("show");
+    document.body.classList.add("show");
+    navBar.classList.add("show");
+  }
+});
+
+navClose.addEventListener("click", () => {
+  if (navLeft < 0) {
+    menu.classList.remove("show");
+    document.body.classList.remove("show");
+    navBar.classList.remove("show");
+  }
+});
+
+
+(function(){
+  const cartBtn = document.querySelectorAll(".bx-shopping-bag");
+
+  cartBtn.forEach(function(btn) {
+    btn.addEventListener('click',function(event){
+      console.log(event.target);
+    });
+  });
+});
